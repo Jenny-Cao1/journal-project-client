831478e8f64aa5db82c9ba0fe631ab0ca266caa3
jncao49 <jncao49@gmail.com>
2022-11-24 13:43:19 -0500

    feat: use heroku app URL instead of localhost

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Grid, CircularProgress } from '@material-ui/core';
+import { Grid, CircularProgress, Typography } from '@material-ui/core';
 
 import Post from './Post/Post';
 import useStyles from './styles';
@@ -10,7 +10,7 @@ const Posts = ({ setCurrentId }) => {
     const classes = useStyles();
 
     return(
-        !posts.length ? <CircularProgress /> : (
+        !posts.length ? <Typography className={classes.title} variant="h3">Create a Post!</Typography> : (
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
                 {posts.map((post) => (
                     <Grid key={post._id} item xs={12} sm={6}>
