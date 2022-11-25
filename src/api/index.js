6ed9aecda8e4b5726a6e73e24ccaa82b0e6943d4
jncao49 <jncao49@gmail.com>
2022-11-25 11:58:30 -0500

    feat: change backend api to one hosted by render (goodbye heroku :( ) )

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://journals-project.herokuapp.com/posts';
+const url = 'https://journals-project.onrender.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 
